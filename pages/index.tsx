b8273b3edbcf5efefe637d335a2629e3601c5a2f
Ninshad <ninshad7@gmail.com>
2022-10-06 15:58:11 +0530

    6/10/22

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,17 @@ const posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Next-Blog-App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => <PostCard post={post} key={post.title} /> )}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
